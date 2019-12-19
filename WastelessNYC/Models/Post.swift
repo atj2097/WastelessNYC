079ffa9af431d59e0c1d619b29582f914e305052
Adam <adamjackson@pursuit.org>
2019-12-19 10:02:08 -0500

    Added Firebase helpers

---
+++ WastelessNYC/Models/Post.swift
@@ -0,0 +1,48 @@
+//
+//  Post.swift
+//  WastelessNYC
+//
+//  Created by God on 12/19/19.
+//  Copyright © 2019 God. All rights reserved.
+//
+
+import Foundation
+import FirebaseFirestore
+
+struct Post {
+    let title: String
+    let body: String
+    let id: String
+    let creatorID: String
+    let dateCreated: Date?
+    
+    init(title: String, body: String, creatorID: String, dateCreated: Date? = nil) {
+        self.title = title
+        self.body = body
+        self.creatorID = creatorID
+        self.id = UUID().description
+        self.dateCreated = dateCreated
+    }
+    
+    init?(from dict: [String: Any], id: String) {
+        guard let title = dict["title"] as? String,
+            let body = dict["body"] as? String,
+            let userID = dict["creatorID"] as? String,
+            let dateCreated = (dict["dateCreated"] as? Timestamp)?.dateValue() else { return nil }
+        
+        self.title = title
+        self.body = body
+        self.creatorID = userID
+        self.id = id
+        self.dateCreated = dateCreated
+    }
+    
+    var fieldsDict: [String: Any] {
+        return [
+            "title": self.title,
+            "body": self.body,
+            "creatorID": self.creatorID
+        ]
+    }
+}
+
