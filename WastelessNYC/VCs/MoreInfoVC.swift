f44b6b418199dff7a369309dad0edb1be272fcb1
Adam <adamjackson@pursuit.org>
2019-12-19 13:19:02 -0500

    Added Nutrition Facts VC

---
+++ WastelessNYC/VCs/MoreInfoVC.swift
@@ -0,0 +1,34 @@
+//
+//  MoreInfoVC.swift
+//  WastelessNYC
+//
+//  Created by God on 12/19/19.
+//  Copyright © 2019 God. All rights reserved.
+//
+
+import UIKit
+
+class MoreInfoVC: UIViewController {
+    @IBOutlet weak var infoText: UITextView!
+    @IBOutlet weak var foodName: UILabel!
+    @IBOutlet weak var extendedExpiration: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
